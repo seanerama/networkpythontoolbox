d00812a3aa3ccc91a1a698e14b75641ce92aac9c
Sean Mahoney <sdmahoney01@gmail.com>
2024-11-26 12:06:24 -0600

    mod part2

--- part2.py
+++ part2.py
@@ -1,19 +1,23 @@
-# We are going to use the ConnectHandler tool from the Netmiko toolbox.
-# It allows us to establish, use, and manage SSH connections with Python.
 from netmiko import ConnectHandler
+
+# We are using the dotenv toolbox to load environment variables.
+# Environment variables are a safe place to store sensitive information like usernames, passwords, and IP addresses.
 from dotenv import load_dotenv
+
+# We are using the os toolbox to retrieve these environment variables once they are loaded.
+# This tool allows us to interact with our computer's operating system, such as accessing files or environment variables.
 import os
 
 # Load the environment variables from the .env file
-# This helps us keep sensitive information like credentials and IP addresses secure.
+# This helps us keep sensitive information like credentials secure.
 load_dotenv()
 
 # Retrieve the credentials and IP address from the .env file
 # Think of the .env file as a drawer where we keep all the important details for our tools.
 device_type = "cisco_ios"
-host = os.getenv("DEVICE_IP")       # The IP address of the router
 username = os.getenv("USERNAME")    # The SSH username
 password = os.getenv("PASSWORD")    # The SSH password
+host = '192.168.1.1'
 
 # Now we calibrate our tool using these details.
 ssh_connection = ConnectHandler(
@@ -23,12 +27,8 @@ ssh_connection = ConnectHandler(
     password=password
 )
 
-# Use the 'send_command' function to send the 'show clock' command to the device.
-# This function asks the device for information and brings the response back.
 output = ssh_connection.send_command("show clock")
 
-# Print the output to the screen so we can see what the device sent back.
 print(output)
 
-# Clean up by closing the SSH session with the 'disconnect' function of our tool.
 ssh_connection.disconnect()
